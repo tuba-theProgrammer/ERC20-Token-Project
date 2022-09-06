335c70e539dfe4bce3f83b772a5fb4931da5e41d
Tuba Asif <68794712+tuba-theProgrammer@users.noreply.github.com>
2022-09-06 17:27:46 +0500

    interface implementation

---
+++ src/contracts/ERC20Project.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+interface ERC20Interface{
+    function totalSupply() external view returns(uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function transfer(address to,uint tokens) external returns(bool success);
+
+
+    // function allowance(address tokenOwner,address spender) external view returns(uint remaining);
+    // function approve(address spender,uint tokens) external returns (bool success);
+    // function tranferFrom(address from,address to,uint tokens) external returns (bool success);
+
+
+    event Tranfer(address indexed from, address indexed to, uint token);
+    //event Approval(address indexed tokenOwner,address indexed spender,uint tokens);
+
+
+
+}
+
+
+contract ERC20 is ERC20Interface{
+    
+    string public name = "Cryptos";
+    string public symbol= "CRPT";
+    uint public decimals = 0; //18
+    // total supply - represent the toral number of tokens
+    uint public override totalSupply; // a getter function will be created bcz the variable is public
+
+    uint public founder;
+    // map to store each address and no of tokens with respect to it
+    mapping(address=>uint) public balances;
+}
